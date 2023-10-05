53eda9f3060ef36e438af74e952aca2598ae3d36
JoseMilans <josemilans@gmail.com>
2023-10-05 18:29:58 +0000

    A class BaseGeometry based on 5-base_geometry.py including a public instance method that raises an exception with a specific message.

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module introducing the BaseGeometry class."""
+
+
+class BaseGeometry:
+    """BaseGeometry class with methods and attributes."""
+
+    def area(self):
+        """Method that raises an exception with a specific message."""
+        raise Exception("area() is not implemented")
