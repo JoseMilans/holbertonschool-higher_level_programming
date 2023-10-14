2576607a22086d9145d4bdd88aa94284ddd65f14
JoseMilans <josemilans@gmail.com>
2023-10-14 17:41:52 +0000

    Feat: Property for getter and setter, methods for *args and **kwarg and return filled dictionary.

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -22,3 +22,39 @@ class Square(Rectangle):
     def __str__(self):
         """Return formatted string"""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    @property
+    def size(self):
+        """Get square size."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Set square size."""
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """
+        Update square attrs using args or kwargs.
+        args order: id, size, x, y.
+        args takes priority over kwargs.
+        """
+        attrs = ['id', 'size', 'x', 'y']
+        if args:
+            for idx, val in enumerate(args):
+                if idx < len(attrs):
+                    setattr(self, attrs[idx], val)
+        else:
+            for key, value in kwargs.items():
+                if key in attrs:
+                    setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Return dictionary representation."""
+        return {
+            'id': self.id,
+            'size': self.size,
+            'x': self.x,
+            'y': self.y
+        }
