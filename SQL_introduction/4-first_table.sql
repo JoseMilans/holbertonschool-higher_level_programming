3aaa42954312a3bc31fbd65b4ea0b61473176c08
JoseMilans <josemilans@gmail.com>
2023-11-11 12:49:31 +0000

    feat: Add script to create first_table with id and name columns

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,6 +1,5 @@
 -- Creates a table first_table in the current database if it doesn't already exist
-CREATE TABLE IF NOT EXISTS first_table
-(
+CREATE TABLE IF NOT EXISTS first_table(
     id INT,
     name VARCHAR(256)
 );
