bf1dab8c89901d8e2995033bf12a21a2ad6b69c3
JoseMilans <josemilans@gmail.com>
2023-11-13 17:54:05 +0000

    feat: Add query to list California cities with subquery

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,6 @@
+-- List all cities from California that can be found in database
+SELECT id,name FROM cities
+WHERE state_id = (
+  SELECT id FROM states WHERE name = 'California'
+)
+ORDER BY cities.id;
