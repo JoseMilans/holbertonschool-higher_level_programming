690c3737311e7668be66da8bd75aedd7a7d1dff2
JoseMilans <josemilans@gmail.com>
2023-11-13 20:07:09 +0000

    fix: Add another explicit ASC hoping addresses issue

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,11 @@
---Display a list of tv show titles and their genre ids
-SELECT tv_shows.title,tv_show_genres.genre_id
-FROM tv_shows
-JOIN tv_show_genres
-WHERE tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title,tv_show_genres.genre_id ASC;
+-- Display a list of tv show titles and their genre ids
+SELECT
+    tv_shows.title,
+    tv_show_genres.genre_id
+FROM
+    tv_shows, tv_show_genres
+WHERE
+    tv_shows.id = tv_show_genres.show_id
+ORDER BY
+    tv_shows.title ASC,
+    tv_show_genres.genre_id ASC;
