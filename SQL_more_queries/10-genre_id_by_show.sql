a27e1dbb8dffc55ef20a781b6819b1c4c362b334
JoseMilans <josemilans@gmail.com>
2023-11-13 19:19:19 +0000

    feat: Add query to list TV shows with genre IDs

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,5 @@
+--Display a list of tv show titles and their genre ids
+SELECT tv_shows.title,tv_show_genres.genre_id 
+FROM tv_shows,tv_show_genres
+WHERE tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title,tv_show_genres.genre_id;
