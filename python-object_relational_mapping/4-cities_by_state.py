7177a11cf028cd282ac09eb7a2177929bf769479
JoseMilans <josemilans@gmail.com>
2023-11-15 17:04:07 +0000

    feat: Add script to list cities with state names

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Module to connect to the MySQL database and list all cities
+from the database hbtn_0e_4_usa along with their corresponding state names
+"""
+
+import MySQLdb
+from sys import argv
+
+
+def list_cities(mysql_user, mysql_pw, db_name):
+    """
+    Displays all cities from the database along with their state names
+    sorted by city IDs
+    """
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=mysql_user, passwd=mysql_pw, db=db_name)
+    try:
+        with db.cursor() as cur:
+            cur.execute("SELECT cities.id, cities.name, states.name "
+                        "FROM cities "
+                        "JOIN states ON cities.state_id = states.id "
+                        "ORDER BY cities.id ASC")
+            for city in cur:
+                print(city)
+    finally:
+        db.close()
+
+
+if __name__ == '__main__':
+    if len(argv) == 4:
+        list_cities(argv[1], argv[2], argv[3])
