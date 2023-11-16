efdf22bb5fe8f0f3ff2cce746564f9acd42a9e46
JoseMilans <josemilans@gmail.com>
2023-11-16 20:57:36 +0000

    feat: Add script to filter states containing 'a' in SQLAlchemy

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Module to list all State objects containing 'a'
+from the database hbtn_0e_6_usa
+"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+
+def list_states_containing_a(mysql_usr, mysql_pw, db_name):
+    """List all states containing the letter 'a
+    """
+    conn = f'mysql+mysqldb://{mysql_usr}:{mysql_pw}@localhost:3306/'
+    engine = create_engine(conn + db_name)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State)\
+                    .filter(State.name.like('%a%'))\
+                    .order_by(State.id)
+    for state in states:
+        print(f'{state.id}: {state.name}')
+
+    session.close()
+
+
+if __name__ == '__main__':
+    if len(argv) == 4:
+        list_states_containing_a(argv[1], argv[2], argv[3])
