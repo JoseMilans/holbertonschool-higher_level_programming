6a4551b3652bfd958c786d0c29e7db15cfb9fb4a
JoseMilans <josemilans@gmail.com>
2024-02-01 00:11:38 +0000

    feat: display fetched hello translation

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', async () => {
+  try {
+    const response = await fetch('https://hellosalut.stefanbohacek.dev/?lang=fr');
+    const data = await response.json();
+    document.getElementById('hello').textContent = data.hello;
+  } catch (error) {
+    console.error('Fetch error:', error);
+  }
+});
